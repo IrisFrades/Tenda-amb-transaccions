73d1dd3414a36899a2aed80059d8f7b8519e2eca
IrisFrades <a24irifragra@inspedralbes.cat>
2026-03-19 20:33:35 +0100

    Ejercicio echo

--- Assets/Scripts/ItemResumePrefab.cs
+++ Assets/Scripts/ItemResumePrefab.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using TMPro.Examples;
 using UnityEngine;
 
 public class ItemResumePrefab : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI textNameItemResume;
+    [SerializeField] private TextMeshProUGUI textQuantityItemResume;
+
+    public ItemSlot itemSlotInstance;
+    public Item itemInstance;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateUI();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateUI()
+    {
+        textNameItemResume.text = itemInstance.nameItem;
+        textQuantityItemResume.text = itemInstance.quantityItem.ToString();
+     
     }
 }
