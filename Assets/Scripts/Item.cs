9e1471803ed85d9713886cf4304829b61fa7d650
IrisFrades <a24irifragra@inspedralbes.cat>
2026-03-19 20:00:07 +0100

    Tienda

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private int IDItem;
+    [SerializeField] public string nameItem;
+    [SerializeField] public Sprite imageItem;
+    public int quantityItem;
+
 }
--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
+    public TextMeshProUGUI textNameItem;
+    public Image imageItem;
+    public Button addButton;
+    public Button subsButton;
+
+    public Item itemInfoInstance;
+
+    private int quantityToAddOrSubs = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!Application.isPlaying) return;
+
+        UpdateUI();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    void UpdateUI()
+    {
+        if(itemInfoInstance == null) { return; }
+
+        textNameItem.text = itemInfoInstance.nameItem;
+        imageItem.sprite = itemInfoInstance.imageItem;
+    }
+
+    public void addQuantityItem()
+    {
+        Debug.Log("Has añadido un elemento mas de: " + itemInfoInstance.nameItem);
+        itemInfoInstance.quantityItem += quantityToAddOrSubs;      
+    }
+
+    public void subsQuantityItem()
+    {
+
+        if (itemInfoInstance.quantityItem <= 0)
+        {
+            Debug.Log("No se pueden quitar mas, no tienes");
+        }
+        else
+        {
+            Debug.Log("Has eliminado un elemento de: " + itemInfoInstance.nameItem);
+            itemInfoInstance.quantityItem -= quantityToAddOrSubs;
+        }
+
+    }
+
 }
