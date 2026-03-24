c04a98e64732bf59aa180923efbd378812c3b5a9
IrisFrades <I306f1780g>
2026-03-24 18:28:17 +0100

    Se puede guardar todo en base de datos

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -16,7 +16,7 @@ public class InventoryManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        LoadInventoryFromDB();
     }
 
     // Update is called once per frame
